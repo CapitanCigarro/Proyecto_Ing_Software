b011410f19d854f651102a6c4a9cc4e9838af39d
Bandido209 <estebananm2005@gmail.com>
2024-11-18 23:08:49 -0300

    Añadiendo pagina de contacto de la institucion, con botón correspondiente.

--- app.py
+++ app.py
@@ -2,12 +2,19 @@ from flask import Flask
 from flask import render_template, redirect, request, Response, session
 from flask_mysqldb import MySQL, MySQLdb
 from Code.Logic.Especialista import Especialista
+import json
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template(('menu_incial.html'))
+
+@app.route('/contacto')
+def contacto():
+    with open('Data/datosCentroMedico.json', 'r', encoding='utf-8') as file:
+        data = json.load(file)
+    return render_template('informacionCentroMedico.html', data=data['institucion'])
 
 @app.route('/agendar-cita', methods=['POST'])
 def agendar_cita():
@@ -25,15 +32,13 @@ def confirmar_cita():
             especialista = Especialista.get_especialista(rut)
             fecha = request.form['fecha']
             hora = request.form['hora']
-            
-            # Procesar la cita como necesites (ej. guardarla o enviar por correo)
+
             return render_template('confirmar_cita.html', especialidad=especialista.especialidad, fecha=fecha, hora=hora, especialista=especialista, nombre=especialista.nombre)
 
         except KeyError as e:
             print(f"Error: El campo {e} no se encuentra en el formulario")
             return "Error: Faltan campos en el formulario", 400
-        
-    # Si es un GET, solo renderiza el formulario
+
     return render_template('agendar_cita.html')
     
 if __name__ == '__main__':
